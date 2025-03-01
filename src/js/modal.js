b64059e3b4a2b370bcffbe93934e5658af000de0
Eks <eks220885@gmail.com>
2025-03-01 15:16:12 +0200

    add validation input name for modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,13 +1,13 @@
-
 document.addEventListener("DOMContentLoaded", () => {
   const modal = document.querySelector("[data-modal]");
-  if (!modal) return; // Калі мадальнага акна няма – выхад
+  if (!modal) return; 
 
   const openBtns = document.querySelectorAll("[data-modal-open]");
   const closeBtns = document.querySelectorAll("[data-modal-close]");
   const backdrop = document.querySelector(".backdrop");
+  const nameInput = document.querySelector(".input-name");
   const emailInput = document.querySelector(".input-email");
-  const sendButton = document.querySelector(".form-btn.send");
+  const sendButton = document.querySelector(".form-btn");
 
   const toggleModal = () => modal.classList.toggle("is-open");
 
@@ -24,11 +24,31 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 
-  function validateEmail() {
-    sendButton.disabled = !emailInput.checkValidity();
-  }
-
-  emailInput.addEventListener("input", validateEmail);
+  nameInput.addEventListener("input", validateInputs);
+  emailInput.addEventListener("input", validateInputs);
 });
 
+function validateInputs() {
+  const nameInput = document.querySelector(".input-name");
+  const emailInput = document.querySelector(".input-email");
+  const isNameValid = nameInput.checkValidity();
+  const isEmailValid = emailInput.checkValidity();
+  const submitButton = document.querySelector('.modal-form-btn');
+  
+  if (isNameValid && isEmailValid) {
+    submitButton.removeAttribute('disabled');
+    submitButton.classList.add('active');
+  } else {
+    submitButton.setAttribute('disabled', 'true');
+    submitButton.classList.remove('active');
+  }
+}
+function closeModal() {
+  document.querySelector("[data-modal]").classList.remove('is-open');
+}
 
+function handleSubmit(event) {
+  event.preventDefault();
+  closeModal();
+  window.location.href = "/"; // Redirect to the main page
+}
